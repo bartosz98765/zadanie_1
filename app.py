419997c2546ab4382b29a6b3998ec5730b17f9f2
bartek <bartek@grupazpr.pl>
2024-06-01 14:36:21 +0200

    konfigracja bazy danych

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
 
+from db import init_db
+
 app = Flask(__name__)
+app.config["API_TITLE"] = "My API"
+app.config["API_VERSION"] = "v1"
+app.config["OPENAPI_VERSION"] = "3.0.2"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///flask_app.db"
+
+db = init_db(app)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return 'Hello World!'
 
 
