1595e3922453808e196e99914723a926a45f3119
bartek <bartek@grupazpr.pl>
2024-06-01 16:49:41 +0200

    przejście na json w odpowiedzi, brak zabezpieczeń

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+from http import HTTPStatus
+
 from flask import Flask, request, url_for
+from flask_smorest import Api, Blueprint
+from marshmallow import Schema, fields
 
 from db import init_db, Tag
 
@@ -8,26 +12,37 @@ app.config["API_VERSION"] = "v1"
 app.config["OPENAPI_VERSION"] = "3.0.2"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///flask_app.db"
 
+api = Api(app)
+name_app = Blueprint("tags", __name__, url_prefix="/v1")
+
 db = init_db(app)
 
 
+class TagSchema(Schema):
+    id = fields.String()
+    name = fields.String()
+
+
+@name_app.route("/name/", methods=["GET"])
+@name_app.response(HTTPStatus.OK, TagSchema)
 @app.route("/tags/<tag_id>/", methods=["GET"])
 def get_tag(tag_id: str):
     tag = db.get_or_404(Tag, tag_id)
-    return f"Tag o id: {tag.id} nazwa: {tag.name}>"
+    return {"id": tag.id, "name": tag.name}
 
 
 @app.route("/tags/", methods=["POST"])
+@name_app.response(HTTPStatus.OK, TagSchema)
 def add_tag():
     name = request.json.get("name")
     if name:
-        p = Tag(name=name)
-        db.session.add(p)
+        tag = Tag(name=name)
+        db.session.add(tag)
         db.session.commit()
 
-    uri = url_for("get_tag", tag_id=p.id)
+    uri = url_for("get_tag", tag_id=tag.id)
 
-    return p.id, 200, {"Location": uri}
+    return {"id": tag.id, "name": tag.name}, {"Location": uri}
 
 
 if __name__ == "__main__":
