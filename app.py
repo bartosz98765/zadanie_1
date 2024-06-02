7303d14d1f78833e3783dc57d8eda9cbf87b36fb
bartek <bartek@grupazpr.pl>
2024-06-02 19:03:25 +0200

    dodany create_app

--- app.py
+++ app.py
@@ -1,70 +1,76 @@
 from http import HTTPStatus
 
-from flask import Flask, request, url_for, abort, make_response
+from flask import request, url_for, make_response, Flask
 from flask_smorest import Api, Blueprint
 
 from db import init_db, Tag
 from settings import (
-    INVALID_DATA_FORMAT_ERROR, TAG_DOES_NOT_EXIST_ERROR,
+    INVALID_DATA_FORMAT_ERROR,
+    TAG_DOES_NOT_EXIST_ERROR,
 )
 from tag_schema import TagRequestSchema, TagSchema, TagIdSchema
 
-app = Flask(__name__)
-app.config["API_TITLE"] = "My API"
-app.config["API_VERSION"] = "v1"
-app.config["OPENAPI_VERSION"] = "3.0.2"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///flask_app.db"
 
-api = Api(app)
-tags_app = Blueprint("tags", __name__, url_prefix="/v1")
-
-db = init_db(app)
+class Config:
+    API_TITLE = "TAGS"
+    API_VERSION = "v1"
+    OPENAPI_VERSION = "3.0.2"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///tags.db"
 
 
-@tags_app.route("/tags/<tag_id>/", methods=["GET"])
-@tags_app.response(HTTPStatus.OK, TagSchema)
-def get_tag(tag_id: str):
-    __validate_tag(tag_id)
-    tag = db.get_or_404(Tag, tag_id)
-    return {"id": tag.id, "name": tag.name}
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tags.db"
+    api = Api(app)
 
+    db = init_db(app)
 
-def __validate_tag(tag_id):
-    TagIdSchema().validate({"id": tag_id})
+    tags_app = Blueprint("tags", __name__, url_prefix="/v1")
 
+    @tags_app.route("/tags/<tag_id>/", methods=["GET"])
+    @tags_app.response(HTTPStatus.OK, TagSchema)
+    def get_tag(tag_id: str):
+        __validate_tag(tag_id)
+        tag = db.get_or_404(Tag, tag_id)
+        return {"id": tag.id, "name": tag.name}
 
-@app.errorhandler(HTTPStatus.NOT_FOUND)
-def tag_does_not_exist_handler(error):
-    return make_response(TAG_DOES_NOT_EXIST_ERROR, HTTPStatus.NOT_FOUND)
+    def __validate_tag(tag_id):
+        TagIdSchema().validate({"id": tag_id})
 
+    @app.errorhandler(HTTPStatus.NOT_FOUND)
+    def tag_does_not_exist_handler(error):
+        return make_response(TAG_DOES_NOT_EXIST_ERROR, HTTPStatus.NOT_FOUND)
 
-@tags_app.route("/tags/", methods=["POST"])
-@tags_app.response(HTTPStatus.OK, TagSchema)
-def add_tag():
-    tag_data = TagRequestSchema().load(request.json)
-    tag = Tag(name=tag_data["name"])
-    db.session.add(tag)
-    db.session.commit()
-
-    uri = url_for("tags.get_tag", tag_id=tag.id)
-    return {"id": tag.id, "name": tag.name}, {"Location": uri}
-
-
-@app.errorhandler(HTTPStatus.UNSUPPORTED_MEDIA_TYPE)
-def invalid_tag_post_request_data_format_handler(error):
-    return make_response(INVALID_DATA_FORMAT_ERROR, HTTPStatus.METHOD_NOT_ALLOWED)
-
-
-@tags_app.route("/tags/<tag_id>/", methods=["DELETE"])
-@tags_app.response(HTTPStatus.NO_CONTENT)
-def delete_tag(tag_id: str):
-    __validate_tag(tag_id)
-    if tag := db.session.get(Tag, tag_id):
-        db.session.delete(tag)
+    @tags_app.route("/tags/", methods=["POST"])
+    @tags_app.response(HTTPStatus.OK, TagSchema)
+    def add_tag():
+        tag_data = TagRequestSchema().load(request.json)
+        tag = Tag(name=tag_data["name"])
+        db.session.add(tag)
         db.session.commit()
 
+        uri = url_for("tags.get_tag", tag_id=tag.id)
+        return {"id": tag.id, "name": tag.name}, {"Location": uri}
 
-api.register_blueprint(tags_app)
+    @app.errorhandler(HTTPStatus.UNSUPPORTED_MEDIA_TYPE)
+    def invalid_tag_post_request_data_format_handler(error):
+        return make_response(INVALID_DATA_FORMAT_ERROR, HTTPStatus.METHOD_NOT_ALLOWED)
+
+    @tags_app.route("/tags/<tag_id>/", methods=["DELETE"])
+    @tags_app.response(HTTPStatus.NO_CONTENT)
+    def delete_tag(tag_id: str):
+        __validate_tag(tag_id)
+        if tag := db.session.get(Tag, tag_id):
+            db.session.delete(tag)
+            db.session.commit()
+
+    api.register_blueprint(tags_app)
+
+    return app
+
+
+app = create_app()
 
 if __name__ == "__main__":
     app.run()
