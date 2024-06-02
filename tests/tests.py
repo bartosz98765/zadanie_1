4cd9af23e85e8964686740a7360e32ca3a206b1a
bartek <bartek@grupazpr.pl>
2024-06-02 19:36:23 +0200

    dodany test_get_tag_returns_bad_request_when_invalid_tag_id - OK

--- tests/tests.py
+++ tests/tests.py
@@ -1,17 +1,17 @@
 from http import HTTPStatus
 
-
 from db import Tag, db
+from settings import INVALID_TAG_ID_ERROR
 
 
-def create_tag(tag_name, db_session):
+def create_tag(tag_name, session):
     tag = Tag(name=tag_name)
-    db_session.add(tag)
-    db_session.commit()
+    session.add(tag)
+    session.commit()
     return tag
 
 
-def test_get_tag(client):
+def test_get_tag_returns_tag_when_exist(client):
     tag = create_tag("Testowy tag", db.session)
     url = f"/v1/tags/{tag.id}/"
 
@@ -20,3 +20,13 @@ def test_get_tag(client):
     assert response.status_code == HTTPStatus.OK
     assert response.json["id"] == tag.id
     assert response.json["name"] == tag.name
+
+
+def test_get_tag_returns_bad_request_when_invalid_tag_id(client):
+    tag_id = "0665ca92-invalid-tag-id"
+    url = f"/v1/tags/{tag_id}/"
+
+    response = client.get(url)
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json == INVALID_TAG_ID_ERROR
